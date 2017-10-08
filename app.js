5441ed83c131e15486ce1c34388ed9931dd7d146
Vergie Ericson T <vergieericsont@gmail.com>
2017-10-08 11:47:54 +0700

    dynamically routing fix

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const express = require('express')'clean:all'
+const express = require('express')
 const fs = require('fs')
 const globalConfig = require('./config/global.json')
 const route = require('./' + globalConfig['file']['route'])
 const app = express()
+app.set('view engine', 'pug')
 
 app.get('*', function (req, res) {
-  if(!route[req.url])res.send('Hello 404!')
-  pageConfig = require('./' + globalConfig['dir']['config-page'] + route[req.url])
+  var routeUrl = req.url + '';
+  if(typeof route[routeUrl] == 'undefined'){
+    if(routeUrl.charAt(routeUrl.length-1) == "/")
+    routeUrl = routeUrl.slice(0, routeUrl.length -1)
+    else
+    routeUrl = routeUrl + "/" ;
+  }
+  if(!route[routeUrl])res.send('Hello 404!')
+  pageConfig = require('./' + globalConfig['dir']['config-page'] + route[routeUrl])
   res.send(pageConfig)
 })
 
